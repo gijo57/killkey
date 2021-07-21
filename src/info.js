184dadcffaa05c267197f7f3f94dcf3909150699
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-07-21 19:54:15 +0200

    fix start screen and ingame info fonts

--- src/info.js
+++ src/info.js
@@ -6,7 +6,7 @@ class Info {
 
   draw(health) {
     this.game.ctx.save();
-    this.game.ctx.font = '40px Arial';
+    this.game.ctx.font = '40px bloodlust expanded';
     this.game.ctx.fillStyle = 'red';
     this.game.ctx.fillText(`Level: ${this.level}`, 200, 35);
     this.game.ctx.fillText(`Health: ${health}`, 400, 35);
