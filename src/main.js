68fcbab0e9d61a65a664f017ee3d697fc30e8ec8
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-07-22 21:39:33 +0200

    add choose map button and separate map list to start screen

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
 const canvas = document.querySelector('canvas');
 
+const mapListElement = document.querySelector('#maps');
+
 const mapList = document.querySelector('ol').children;
-let mapNumber = 1;
+
+const chosenMap = document.querySelector('#chosen');
+
+let mapNumber;
 
 for (let i = 0; i < mapList.length; i++) {
   mapList[i].addEventListener('click', (e) => {
+    startButton.style.display = '';
+    mapButton.style.display = '';
+    mapListElement.style.display = 'none';
+    chosenMap.style.display = 'flex';
     mapNumber = Number(e.target.id);
+    chosenMap.querySelector('span').innerText = mapNumber;
   });
 }
 
 const startScreen = document.querySelector('#start');
 const startButton = document.querySelector('#btn-start');
+const mapButton = document.querySelector('#btn-map');
 
 const gameScreen = document.querySelector('#game');
 
@@ -23,4 +34,11 @@ const game = new Game(canvas, screens, tryAgainButton);
 
 startButton.addEventListener('click', () => {
   game.start();
+});
+
+mapButton.addEventListener('click', () => {
+  mapListElement.style.display = 'flex';
+  startButton.style.display = 'none';
+  mapButton.style.display = 'none';
+  chosenMap.style.display = 'none';
 });
